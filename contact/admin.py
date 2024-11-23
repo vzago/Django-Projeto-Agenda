f929a4993229470aa552f6e0ce9e3b9b00848376
vzago <vitorzago94@gmail.com>
2024-11-23 11:00:34 -0300

    Creating the template, view, and URL to show a unique contact

--- contact/admin.py
+++ contact/admin.py
@@ -6,7 +6,7 @@ from contact.models import Category
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'phone', 'email', 'created_date', 'show',
-    ordering = 'id', #-id para decrescente
+    ordering = '-id', #-id para decrescente
     #list_filter = 'created_date', # ou ('created_date',)
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
