f88a8cd8c9eb009e3e4e799174fad97419e77b8a
vzago <vitorzago94@gmail.com>
2024-11-23 11:44:20 -0300

    Creating partial files to use with include in models

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,6 +6,7 @@ def index(request):
     
     context = {
         'contacts': contacts,
+        'site_title' : 'Contatos - ',
     }
     
     return render(
@@ -16,8 +17,13 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.get(pk = contact_id)
     single_contact = get_object_or_404(Contact, pk=contact_id,show=True)
+    
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -'
+    
     context = {
         'contact': single_contact,
+        'site_title' : site_title,
+        
     }
     
     return render(
