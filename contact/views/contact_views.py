00f1928214d6ad27d9fadd5f06fa1faf645b4a94
vzago <vitorzago94@gmail.com>
2024-11-25 10:36:53 -0300

    Filtering values with Q and OR for search field

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from contact.models import Contact
-from django.http import Http404;
+from django.db.models import Q
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
     
     context = {
         'contacts': contacts,
@@ -14,6 +14,33 @@ def index(request):
         'contact/index.html',
         context
     )
+def search(request):
+    search_value = request.GET.get('q', '').strip() # Get the search value from the request
+    
+    if search_value == '':
+        return redirect('contact:index') # Redirect to index page
+    
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains= search_value) | # Search in first_name or
+            Q(last_name__icontains= search_value) | # Search in last_name
+            Q(phone__icontains= search_value) | # Search in last_name
+            Q(email__icontains= search_value)  # Search in email
+            )\
+        .order_by('-id')    
+    
+    context = {
+        'contacts': contacts,
+        'site_title' : 'Search - ', 
+    }
+    
+    return render( 
+        request,
+        'contact/index.html',
+        context
+    ) # render the index page with the context
+    
 def contact(request, contact_id):
     # single_contact = Contact.objects.get(pk = contact_id)
     single_contact = get_object_or_404(Contact, pk=contact_id,show=True)
