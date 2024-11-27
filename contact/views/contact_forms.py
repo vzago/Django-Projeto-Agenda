bdd46f334937ce35a6e75103e48683f79b070a72
vzago <vitorzago94@gmail.com>
2024-11-27 10:30:09 -0300

    Creating nonfield errors and validating them

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,6 +3,7 @@ from contact.models import Contact
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django import forms
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -10,7 +11,24 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
         )
-
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            None,
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 
 
